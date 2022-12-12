d84dc59b88ed292a921fb1b86723a0189ba56b3e
4l3x <alexander.auermaa@gmail.com>
2022-12-13 00:19:27 +0100

    Global navbar added

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,32 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { LinkContainer } from "react-router-bootstrap";
+
+const AppBar = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand>Property Manager</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/admin">
+              <Nav.Link>Admin Dashboard</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/admin/overview">
+              <Nav.Link>Admin Overview</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default AppBar;
