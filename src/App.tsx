24034a0e4e3c96cf039ee92bd3a12f6892319fed
4l3x <alexander.auermaa@gmail.com>
2022-12-13 00:19:17 +0100

    Bootstrap import added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import AdminDashboard from "./pages/AdminDashboard";
