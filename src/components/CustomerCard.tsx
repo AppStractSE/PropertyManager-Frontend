5f473c3d1e9eeae111c70762b21fa2fd2a066f2c
4l3x <alexander.auermaa@gmail.com>
2022-12-14 00:18:01 +0100

    OnClick will take the user to CustomerID page

--- src/components/CustomerCard.tsx
+++ src/components/CustomerCard.tsx
@@ -1,6 +1,7 @@
 import { Card, Container, ProgressBar } from "react-bootstrap";
 import { BsChevronRight } from "react-icons/bs";
 import { MdLocationOn } from "react-icons/md";
+import { Link } from "react-router-dom";
 import { Customer } from "../models/Customer";
 
 interface Props {
@@ -8,28 +9,30 @@ interface Props {
 }
 const CustomerCard = ({ customer }: Props) => {
   return (
-    <Card onClick={() => console.log(`${customer.name}`)}>
-      <Card.Header className='d-flex align-items-center'>
-        <Container>
-          <Card.Title>{customer.name}</Card.Title>
-          <Container className='d-flex align-items-center'>
-            <MdLocationOn size={22} />
-            <Card.Text className='ms-1'>{customer.address}</Card.Text>
+    <Link to={`/customer/${customer.id}`} className='router-link'>
+      <Card onClick={() => console.log(`${customer.name}`)}>
+        <Card.Header className='d-flex align-items-center'>
+          <Container>
+            <Card.Title>{customer.name}</Card.Title>
+            <Container className='d-flex align-items-center'>
+              <MdLocationOn size={22} />
+              <Card.Text className='ms-1'>{customer.address}</Card.Text>
+            </Container>
           </Container>
-        </Container>
-        <BsChevronRight size={24} />
-      </Card.Header>
-      <Card.Body>
-        <Container>
-          <Card.Text>Antalet uppgifter denna vecka</Card.Text>
-          <ProgressBar>
-            <ProgressBar variant='success' now={25} key={1} label='1' />
-            <ProgressBar variant='warning' now={25} key={2} label='1' />
-            <ProgressBar variant='danger' now={50} key={3} label='4' />
-          </ProgressBar>
-        </Container>
-      </Card.Body>
-    </Card>
+          <BsChevronRight size={24} />
+        </Card.Header>
+        <Card.Body>
+          <Container>
+            <Card.Text>Antalet uppgifter denna vecka</Card.Text>
+            <ProgressBar>
+              <ProgressBar variant='success' now={25} key={1} label='1' />
+              <ProgressBar variant='warning' now={25} key={2} label='1' />
+              <ProgressBar variant='danger' now={50} key={3} label='4' />
+            </ProgressBar>
+          </Container>
+        </Card.Body>
+      </Card>
+    </Link>
   );
 };
 
