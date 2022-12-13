834230e47d10dcad02cc462aaa36ecfc5a90280d
4l3x <alexander.auermaa@gmail.com>
2022-12-14 00:17:24 +0100

    CustomerCard is now it's own component

---
+++ src/components/CustomerCard.tsx
@@ -0,0 +1,36 @@
+import { Card, Container, ProgressBar } from "react-bootstrap";
+import { BsChevronRight } from "react-icons/bs";
+import { MdLocationOn } from "react-icons/md";
+import { Customer } from "../models/Customer";
+
+interface Props {
+  customer: Customer;
+}
+const CustomerCard = ({ customer }: Props) => {
+  return (
+    <Card onClick={() => console.log(`${customer.name}`)}>
+      <Card.Header className='d-flex align-items-center'>
+        <Container>
+          <Card.Title>{customer.name}</Card.Title>
+          <Container className='d-flex align-items-center'>
+            <MdLocationOn size={22} />
+            <Card.Text className='ms-1'>{customer.address}</Card.Text>
+          </Container>
+        </Container>
+        <BsChevronRight size={24} />
+      </Card.Header>
+      <Card.Body>
+        <Container>
+          <Card.Text>Antalet uppgifter denna vecka</Card.Text>
+          <ProgressBar>
+            <ProgressBar variant='success' now={25} key={1} label='1' />
+            <ProgressBar variant='warning' now={25} key={2} label='1' />
+            <ProgressBar variant='danger' now={50} key={3} label='4' />
+          </ProgressBar>
+        </Container>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default CustomerCard;
