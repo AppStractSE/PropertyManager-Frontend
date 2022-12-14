00eaa00b447e2f2323a53ca857dc8397d6575982
4l3x <alexander.auermaa@gmail.com>
2022-12-14 19:43:16 +0100

    Using modal in ChoreCard

--- src/components/ChoreCard.tsx
+++ src/components/ChoreCard.tsx
@@ -1,24 +1,34 @@
+import { useState } from "react";
 import { Card, Container } from "react-bootstrap";
 import { Chore } from "../models/Chore";
+import ChoreInfo from "./modals/ChoreInfo";
 
 interface Props {
   chore: Chore;
 }
 
 const ChoreCard = ({ chore }: Props) => {
+  const [modalShow, setModalShow] = useState(false);
   return (
-    <Card onClick={() => console.log(`Open modal for ${chore.name}`)}>
-      <Card.Body>
-        <Container className='d-flex align-items-center'>
-          <Container>
-            <Card.Text>{chore.name}</Card.Text>
+    <>
+      <Card onClick={() => setModalShow(true)}>
+        <Card.Body>
+          <Container className='d-flex align-items-center'>
+            <Container>
+              <Card.Text>{chore.name}</Card.Text>
+            </Container>
+            {/* Make into component */}
+            <div style={{ height: 12, width: 12, background: "red", borderRadius: "50%" }}></div>
+            {/* Make into component */}
           </Container>
-          {/* Make into component */}
-          <div style={{ height: 12, width: 12, background: "red", borderRadius: "50%" }}></div>
-          {/* Make into component */}
-        </Container>
-      </Card.Body>
-    </Card>
+        </Card.Body>
+      </Card>
+      <ChoreInfo
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        chore={chore}
+      />
+    </>
   );
 };
 
