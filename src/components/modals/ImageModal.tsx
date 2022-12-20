79ed7d4fe8e0c1d030b0a91c70b13ee503b479bd
4l3x <alexander.auermaa@gmail.com>
2022-12-20 03:52:10 +0100

    Clicking chosen photo will a open modal with the photo in it

---
+++ src/components/modals/ImageModal.tsx
@@ -0,0 +1,21 @@
+import { Modal } from "react-bootstrap";
+import { HiDownload } from "react-icons/hi";
+
+const ImageModal = (props: any) => {
+  return (
+    <Modal {...props} fullscreen id='image-modal'>
+      <Modal.Header closeButton>
+        <Modal.Title id='contained-modal-title-vcenter'>Uppladdad bild</Modal.Title>
+        <HiDownload size={28} />
+      </Modal.Header>
+      <Modal.Body>
+        <img
+          className="uploaded-image"
+          src={props.image}
+        />
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default ImageModal;
