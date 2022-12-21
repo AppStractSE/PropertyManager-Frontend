1d5843d49bf6cf9fe792120b41eee0a4fbb83ea0
4l3x <alexander.auermaa@gmail.com>
2022-12-21 03:18:45 +0100

    Toast added to ChoreInfo

--- src/components/modals/ChoreInfo.tsx
+++ src/components/modals/ChoreInfo.tsx
@@ -1,43 +1,71 @@
-import { Form, Modal } from "react-bootstrap";
+import { useState } from "react";
+import { Form, Modal, Toast, ToastContainer } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+import { AiOutlineCheckCircle } from "react-icons/ai";
 import { BsCameraFill } from "react-icons/bs";
-
 const ChoreInfo = (props: any) => {
+  const [show, setShow] = useState(false);
   return (
-    <Modal {...props} size='lg' aria-labelledby='contained-modal-title-vcenter' centered>
-      <Modal.Header closeButton>
-        <Modal.Title>{props.chore.name}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div className='modal-body-section'>
-          <Modal.Title className='p small'>Status</Modal.Title>
-          <div className='p'>Ej påbörjad</div>
-        </div>
-        <div className='modal-body-section'>
-          <Modal.Title className='p small'>Återkommer</Modal.Title>
-          <div className='p'>4 gånger denna månad</div>
-        </div>
-        <div className='modal-body-section'>
-          <Modal.Title className='p small'>Beskrivning</Modal.Title>
-          <div className='p'>{props.chore.description}</div>
-        </div>
-        <div className='modal-body-section'>
-          <div className='d-flex align-items-center camera-container'>
-            <Button>
-              <BsCameraFill size={24} />
-            </Button>
-            <Form className='ms-1'>
-              <Form.Group controlId='formComment'>
-                <Form.Control type='text' placeholder='Lägg till en kommentar...' />
-              </Form.Group>
-            </Form>
+    <>
+      <Modal {...props} size='lg' aria-labelledby='contained-modal-title-vcenter' centered>
+        <Modal.Header closeButton>
+          <Modal.Title>{props.chore.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className='modal-body-section'>
+            <Modal.Title className='p small'>Status</Modal.Title>
+            <div className='p'>Ej påbörjad</div>
           </div>
-        </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Markera som klar</Button>
-      </Modal.Footer>
-    </Modal>
+          <div className='modal-body-section'>
+            <Modal.Title className='p small'>Återkommer</Modal.Title>
+            <div className='p'>4 gånger denna månad</div>
+          </div>
+          <div className='modal-body-section'>
+            <Modal.Title className='p small'>Beskrivning</Modal.Title>
+            <div className='p'>{props.chore.description}</div>
+          </div>
+          <div className='modal-body-section'>
+            <div className='d-flex align-items-center camera-container'>
+              <Button>
+                <BsCameraFill size={24} />
+              </Button>
+              <Form className='ms-1'>
+                <Form.Group controlId='formComment'>
+                  <Form.Control type='text' placeholder='Lägg till en kommentar...' />
+                </Form.Group>
+              </Form>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            onClick={() => {
+              setShow(true);
+              props.onHide();
+            }}
+          >
+            Markera som klar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <ToastContainer position='bottom-center' className='p-3 slide-left'>
+        <Toast
+          className='slide-up'
+          onClose={() => setShow(false)}
+          show={show}
+          delay={8000}
+          autohide
+        >
+          <Toast.Header>
+            <AiOutlineCheckCircle size={28} className='me-2' />
+            <strong className='me-auto'>Syssla klar</strong>
+            <small>Just nu</small>
+          </Toast.Header>
+          <Toast.Body>Bra jobbat! 👏</Toast.Body>
+        </Toast>
+      </ToastContainer>
+    </>
   );
 };
 
