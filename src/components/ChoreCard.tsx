7bca0f873b5703654bc0fb767766265416a10e99
Alexander Auermaa <alexander.auermaa@gmail.com>
2023-01-04 23:32:12 +0100

    ChoreCard now has a better look

--- src/components/ChoreCard.tsx
+++ src/components/ChoreCard.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { Card } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
+import { BsChevronRight } from "react-icons/bs";
 import { CustomerChore } from "../models/CustomerChore";
 import ChoreInfo from "./modals/ChoreInfo";
 
@@ -12,14 +13,16 @@ const ChoreCard = ({ customerchore }: Props) => {
   return (
     <>
       <Card onClick={() => setModalShow(true)}>
+        <Card.Header className='d-flex align-items-center'>
+          <Container>
+            <Card.Title>{customerchore.chore.title}</Card.Title>
+            <Card.Title className='small text-muted'>Planteringsytor</Card.Title>
+          </Container>
+          <BsChevronRight size={24} />
+        </Card.Header>
         <Card.Body>
-          <Card.Text as='h5'>{customerchore.chore.title}</Card.Text>
-          <Card.Text
-            as='p'
-            className='small p-1 status'
-          >
-            Ej påbörjad
-          </Card.Text>
+          <Card.Title className='small text-muted'>Status</Card.Title>
+          <Card.Text className='small p-2 status'>Ej påbörjad</Card.Text>
         </Card.Body>
       </Card>
       <ChoreInfo
