a9dc96172e3203e4e464dc093ad8f779cfa33cab
4l3x <alexander.auermaa@gmail.com>
2023-01-20 17:50:20 +0100

    Basic admin dashboard added Co-authored-by: Lucas Benjaminson <LucBen93@users.noreply.github.com>

---
+++ src/components/modals/AddAreaModal.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { useMutation } from "react-query";
+import axiosClient from "../../utils/axiosClient";
+
+const AddAreaModal = (props: any) => {
+  const [areaValue, setAreaValue] = useState("");
+  const { mutate: postArea, isLoading: postingArea } = useMutation(
+    async () => {
+      return await axiosClient.post("/Area", {
+        name: areaValue,
+      });
+    },
+    {
+      onSuccess: () => {
+        setAreaValue("");
+            console.log("success");
+            props.onHide();
+      },
+    },
+    );
+
+  return (
+    <Modal {...props}>
+      <Modal.Header closeButton>
+        <Modal.Title id='contained-modal-title-vcenter'>Lägg till område</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group className='mb-3' controlId='formAddArea'>
+            <Form.Label>Area</Form.Label>
+            <Form.Control type='text' placeholder='Enter area name' value={areaValue} onChange={(e) => setAreaValue(e.target.value)} />
+          </Form.Group>
+                  <Button className="w-100"
+                      onClick={() => postArea()}
+                      disabled={areaValue.length < 3 ? true : false}
+                  >Lägg till område</Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default AddAreaModal;
