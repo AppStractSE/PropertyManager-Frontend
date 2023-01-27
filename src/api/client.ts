de44a94fbc20b2db975c2928fdeac963d5d96fc5
4l3x <alexander.auermaa@gmail.com>
2023-01-27 23:53:09 +0100

    Endpoint for getting all teammembers a user is on added

--- src/api/client.ts
+++ src/api/client.ts
@@ -1348,6 +1348,43 @@ export class Client extends BaseClient {
         }
         return Promise.resolve<TeamMemberResponseDto>(null as any);
     }
+
+    teamMember_GetTeamMembersByUserId(id: string | undefined): Promise<TeamMemberResponseDto[]> {
+        let url_ = this.baseUrl + "/api/v1/TeamMember/GetTeamMembersByUserId?";
+        if (id === null)
+            throw new Error("The parameter 'id' cannot be null.");
+        else if (id !== undefined)
+            url_ += "Id=" + encodeURIComponent("" + id) + "&";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_: RequestInit = {
+            method: "GET",
+            headers: {
+                "Accept": "application/json"
+            }
+        };
+
+        return this.http.fetch(url_, options_).then((_response: Response) => {
+            return this.processTeamMember_GetTeamMembersByUserId(_response);
+        });
+    }
+
+    protected processTeamMember_GetTeamMembersByUserId(response: Response): Promise<TeamMemberResponseDto[]> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            result200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver) as TeamMemberResponseDto[];
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<TeamMemberResponseDto[]>(null as any);
+    }
 }
 
 export interface Area {
