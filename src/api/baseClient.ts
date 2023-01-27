e5982d050ea573e99591fa988cb20d374d8b617d
4l3x <alexander.auermaa@gmail.com>
2023-01-27 13:10:00 +0100

    https used rather than http

--- src/api/baseClient.ts
+++ src/api/baseClient.ts
@@ -2,5 +2,5 @@ import.meta.env.DEV;
 
 export class BaseClient {
   getBaseUrl = (_: string, __: string | undefined) =>
-    import.meta.env.DEV ? "http://localhost:7178" : "http://legitURL.com";
+    import.meta.env.DEV ? "https://localhost:7178" : "http://legitURL.com";
 }
