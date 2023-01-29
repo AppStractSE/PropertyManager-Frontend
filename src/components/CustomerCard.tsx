80743d5789529b53113fa5fd0e745c7bd1371dd8
4l3x <alexander.auermaa@gmail.com>
2023-01-29 12:26:20 +0100

    Frontend is now up to date

--- src/components/CustomerCard.tsx
+++ src/components/CustomerCard.tsx
@@ -1,34 +1,32 @@
-import { Card, Container, ProgressBar } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
 import { BsChevronRight } from "react-icons/bs";
 import { MdLocationOn } from "react-icons/md";
 import { Link } from "react-router-dom";
-import { CustomerResponseDto } from "../api/client";
+import { UserCustomerData } from "../api/client";
 
 interface Props {
-  customer: CustomerResponseDto;
+  customer: UserCustomerData;
 }
 const CustomerCard = ({ customer }: Props) => {
+  // console.log(customer.customerChores);
   return (
-    <Link to={`/customer/${customer.id}`} className='router-link'>
+    <Link to={`/customer/${customer.customerId}`} className='router-link'>
       <Card>
         <Card.Header className='d-flex align-items-center'>
           <Container>
-            <Card.Title>{customer.name}</Card.Title>
+            <Card.Title>{customer.customerName}</Card.Title>
             <Container className='d-flex align-items-center'>
               <MdLocationOn size={22} />
-              <Card.Text className='ms-1'>{customer.address}</Card.Text>
+              <Card.Text className='ms-1'>{customer.customerAddress}</Card.Text>
             </Container>
           </Container>
           <BsChevronRight size={24} />
         </Card.Header>
         <Card.Body>
           <Container>
-            <Card.Text>Antalet uppgifter denna vecka</Card.Text>
-            <ProgressBar>
-              <ProgressBar variant='success' now={25} key={1} label='1' />
-              <ProgressBar variant='warning' now={25} key={2} label='1' />
-              <ProgressBar variant='danger' now={50} key={3} label='4' />
-            </ProgressBar>
+            {/* Temp */}
+            <Card.Text>Uppgifter: {customer.customerChores!.length}st</Card.Text>
+            {/* Temp */}
           </Container>
         </Card.Body>
       </Card>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,43 +1,42 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
+import { Container } from "react-bootstrap";
 import { useQuery } from "react-query";
-import { Client, TeamMemberResponseDto } from "../api/client";
+import { container, item } from "../animation";
+import { Client, UserCustomerData, UserDataResponseDto, UserTeamData } from "../api/client";
+import CustomerCard from "../components/CustomerCard";
+import SearchAndFilter from "../components/SearchAndFilter";
+import HomePageSkeleton from "../components/skeletons/CustomerPageSkeleton";
 import { useUser } from "../contexts/UserContext";
 
 const Home = () => {
+  const [searchValue, setSearchValue] = useState("");
   const { currentUser } = useUser();
   const client = new Client();
-  console.log(currentUser.userId);
 
-  const { data: teamMembers, isLoading: teamMembersLoading } = useQuery<TeamMemberResponseDto[]>(
-    ["teamMembers", currentUser.userId],
-    async () => client.teamMember_GetTeamMembersByUserId(currentUser.userId!),
+  const { data: userData, isLoading: userDataIsLoading } = useQuery<UserDataResponseDto>(
+    ["userData", currentUser.userId],
+    async () => client.userData_GetUserDataById(currentUser.userId!),
   );
-  if (!teamMembersLoading) console.log(teamMembers);
 
-  // const { data: team } = useQuery<TeamResponseDto>(["team", customer.teamId], async () =>
-  //   client.team_GetTeamById(customer.teamId!),
-  // );
-
-  // const [searchValue, setSearchValue] = useState("");
-  // const fetchCustomers = useAxios({ url: "/Customer", method: "get" });
-  // const { data, error, isLoading } = useQuery<CustomerResponseDto[]>("customers", fetchCustomers);
-  // const filterSearch = data?.filter((customer) =>
+  // Needs to be redone
+  // const filterSearch = userData?.userTeamsData.userCustomersData.filter((customer) =>
   //   customer.name.toLowerCase().includes(searchValue.toLowerCase()),
   // );
-  // console.log(currentUser.userId);
+    // Needs to be redone
 
-  // if (isLoading || filterSearch === undefined || error) {
-  //   return (
-  //     <motion.div
-  //       initial={{ opacity: 0 }}
-  //       animate={{ opacity: 1 }}
-  //       exit={{ opacity: 0 }}
-  //       transition={{ duration: 0.5, ease: "easeInOut" }}
-  //     >
-  //       <HomePageSkeleton />
-  //     </motion.div>
-  //   );
-  // }
+  if (userDataIsLoading) {
+    return (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5, ease: "easeInOut" }}
+      >
+        <HomePageSkeleton />
+      </motion.div>
+    );
+  }
 
   return (
     <motion.div
@@ -46,8 +45,7 @@ const Home = () => {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5, ease: "easeInOut" }}
     >
-      asd
-      {/* <Container className='mt-3 mb-3'>
+      <Container className='mt-3 mb-3'>
         <motion.div
           variants={container}
           initial='hidden'
@@ -55,19 +53,19 @@ const Home = () => {
           className='vstack gap-2 minBreakpoint-xs'
         >
           <div className='h3 mb-0'>Mina kunder</div>
-          <p>{currentUser.displayName}</p>
-          <SearchAndFilter
-            value={searchValue}
-            onChange={setSearchValue}
-            filterSearch={filterSearch.length}
-          />
-          {filterSearch.map((customer) => (
-            <motion.div variants={item} key={customer.id}>
-              <CustomerCard customer={customer} />
-            </motion.div>
+          <SearchAndFilter value={searchValue} onChange={setSearchValue} filtersearch={999} />
+          {userData?.userTeamsData?.map((team: UserTeamData) => (
+            <div key={team.teamId} className='vstack gap-2 minBreakpoint-xs'>
+              <div>{team.teamName}</div>
+              {team?.userCustomersData?.map((customer: UserCustomerData) => (
+                <motion.div variants={item} key={customer.customerId}>
+                  <CustomerCard customer={customer} />
+                </motion.div>
+              ))}
+            </div>
           ))}
         </motion.div>
-      </Container> */}
+      </Container>
     </motion.div>
   );
 };
