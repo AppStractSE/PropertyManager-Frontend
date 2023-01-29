182698f0ace0f221c63d9f6a97d490cab7e4e222
Lucas Benjaminson <lucas_benjaminson@hotmail.com>
2023-01-29 20:21:39 +0100

    Customerchores can now be added to customers

--- src/components/modals/CustomerChore/AddCustomerChores.tsx
+++ src/components/modals/CustomerChore/AddCustomerChores.tsx
@@ -3,12 +3,11 @@ import { Button, Form, Modal } from "react-bootstrap";
 import { useMutation } from "react-query";
 import axiosClient from "../../../utils/axiosClient";
 
-const AddCustomerModal = (props: any) => {
+const AddCustomerChoreModal = (props: any) => {
   const [choreValue, setChoreValue] = useState("");
   const [customerValue, setCustomerValue] = useState("");
   const [periodicValue, setPeriodicValue] = useState("");
   const [frequencyValue, setFrequencyValue] = useState("");
-  // Get;a en customerById
   const { mutate: postCustomerChore, isLoading: postingCustomerChore } = useMutation(
     async () => {
       return await axiosClient.post("/CustomerChore", {
@@ -26,8 +25,6 @@ const AddCustomerModal = (props: any) => {
       },
     },
   );
-
-  // const { data: customerChore } = useQuery<PostCustomerChoreRequestDto>;
   return (
     <Modal {...props} centered>
       <Modal.Header closeButton>
@@ -35,21 +32,76 @@ const AddCustomerModal = (props: any) => {
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Group className='mb-3' controlId='formAddArea'>
+          <Form.Group className='mb-3 d-flex flex-column gap-3' controlId='formAddArea'>
             <Form.Label>Hur ofta ska sysslan utföras?</Form.Label>
             <Form.Control
               type='number'
-              // placeholder=''
               value={frequencyValue}
               onChange={(e) => setFrequencyValue(e.target.value)}
             />
+            <Form.Select
+              aria-label='Chore'
+              value={choreValue}
+              onChange={(e) => setChoreValue(e.target.value)}
+              className='form-active'
+            >
+              <option>Välj syssla</option>
+              {props.chores &&
+                props.chores.map((chore: any) => {
+                  return (
+                    <option key={chore.id} value={chore.id}>
+                      {chore.title}
+                    </option>
+                  );
+                })}
+            </Form.Select>
+            <Form.Select
+              aria-label='Tidsintervall'
+              value={periodicValue}
+              onChange={(e) => setPeriodicValue(e.target.value)}
+              className='form-active'
+            >
+              <option>Välj tidsintervall</option>
+              {props.periodics &&
+                props.periodics.map((periodic: any) => {
+                  return (
+                    <option key={periodic.id} value={periodic.id}>
+                      {periodic.name}
+                    </option>
+                  );
+                })}
+            </Form.Select>
+            <Form.Select
+              aria-label='Välj kund dropdown'
+              value={customerValue}
+              onChange={(e) => setCustomerValue(e.target.value)}
+              className='form-active'
+            >
+              <option>Välj kund</option>
+              {props.customers &&
+                props.customers.map((customer: any) => {
+                  return (
+                    <option key={customer.id} value={customer.id}>
+                      {customer.name}
+                    </option>
+                  );
+                })}
+            </Form.Select>
           </Form.Group>
           <Button
             className='w-100'
             onClick={() => postCustomerChore()}
-            disabled={frequencyValue.length < 3 ? true : false}
+            disabled={
+              choreValue.includes("Välj")
+                ? true
+                : false || periodicValue.includes("Välj")
+                ? true
+                : false || customerValue.includes("Välj")
+                ? true
+                : false || frequencyValue == null
+            }
           >
-            Lägg till område
+            Lägg till syssla på kund
           </Button>
         </Form>
       </Modal.Body>
@@ -57,4 +109,4 @@ const AddCustomerModal = (props: any) => {
   );
 };
 
-export default AddCustomerModal;
+export default AddCustomerChoreModal;
