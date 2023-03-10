279a8f86847db18484368dae93a63b629e7509e0
Zaai90 <erik.grune@gmail.com>
2023-03-10 18:04:20 +0100

    Database fixes

--- src/api/client.ts
+++ src/api/client.ts
@@ -583,6 +583,50 @@ export class Client extends BaseClient {
         return Promise.resolve<ChoreCommentResponseDto[]>(null as any);
     }
 
+    choreComment_DeleteTeamById(id: string | undefined): Promise<FileResponse> {
+        let url_ = this.baseUrl + "/api/v1/ChoreComment/DeleteChoreCommentById?";
+        if (id === null)
+            throw new Error("The parameter 'id' cannot be null.");
+        else if (id !== undefined)
+            url_ += "Id=" + encodeURIComponent("" + id) + "&";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_: RequestInit = {
+            method: "DELETE",
+            headers: {
+                "Accept": "application/octet-stream"
+            }
+        };
+
+        return this.transformOptions(options_).then(transformedOptions_ => {
+            return this.http.fetch(url_, transformedOptions_);
+        }).then((_response: Response) => {
+            return this.processChoreComment_DeleteTeamById(_response);
+        });
+    }
+
+    protected processChoreComment_DeleteTeamById(response: Response): Promise<FileResponse> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200 || status === 206) {
+            const contentDisposition = response.headers ? response.headers.get("content-disposition") : undefined;
+            let fileNameMatch = contentDisposition ? /filename\*=(?:(\\?['"])(.*?)\1|(?:[^\s]+'.*?')?([^;\n]*))/g.exec(contentDisposition) : undefined;
+            let fileName = fileNameMatch && fileNameMatch.length > 1 ? fileNameMatch[3] || fileNameMatch[2] : undefined;
+            if (fileName) {
+                fileName = decodeURIComponent(fileName);
+            } else {
+                fileNameMatch = contentDisposition ? /filename="?([^"]*?)"?(;|$)/g.exec(contentDisposition) : undefined;
+                fileName = fileNameMatch && fileNameMatch.length > 1 ? fileNameMatch[1] : undefined;
+            }
+            return response.blob().then(blob => { return { fileName: fileName, data: blob, status: status, headers: _headers }; });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<FileResponse>(null as any);
+    }
+
     chore_GetAllChores(): Promise<ChoreResponseDto[]> {
         let url_ = this.baseUrl + "/api/v1/Chore";
         url_ = url_.replace(/[?&]$/, "");
