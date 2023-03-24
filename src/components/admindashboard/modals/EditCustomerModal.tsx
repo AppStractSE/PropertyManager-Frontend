47c166cf0647de5016dba8df28bfe491f61fa9ec
Lucas Benjaminson <lucas_benjaminson@hotmail.com>
2023-03-24 15:19:59 +0100

    Customer can now be deleted but it won't cascade delete relative objects like customer chores, stauses and comments

--- src/components/admindashboard/modals/EditCustomerModal.tsx
+++ src/components/admindashboard/modals/EditCustomerModal.tsx
@@ -1,11 +1,14 @@
-import { Modal, Nav, Tab } from "react-bootstrap";
+import { Button, Modal, Nav, Tab } from "react-bootstrap";
+import { BsFillTrashFill } from "react-icons/bs";
+import { useMutation, useQueryClient } from "react-query";
 import {
   CustomerChoreResponseDto,
   CustomerResponseDto,
   Periodic,
   TeamMemberResponseDto,
-  TeamResponseDto
+  TeamResponseDto,
 } from "../../../api/client";
+import { useClient } from "../../../contexts/ClientContext";
 import EditCustomerChores from "../customer/edit/EditCustomerChoreTable";
 import EditCustomer from "../customer/EditCustomer";
 
@@ -28,10 +31,32 @@ const EditCustomerModal = ({
   show,
   onHide,
 }: Props) => {
+  const client = useClient();
+  const queryClient = useQueryClient();
+  const {
+    mutate: deleteCustomer,
+    error: deleteCustomerError,
+    isLoading: deleteCustomerIsLoading,
+  } = useMutation(
+    async () => await client.customer_DeleteCustomerById(customer.id),
+
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("customers");
+        queryClient.invalidateQueries("customerchores");
+        queryClient.invalidateQueries("choreStatuses");
+        queryClient.invalidateQueries("chorecomments");
+      },
+    },
+  );
+
   return (
     <Modal show={show} id='editCustomer' size='lg' onEscapeKeyDown={() => onHide()}>
       <Modal.Header closeButton onHide={() => onHide()}>
         <Modal.Title id='contained-modal-title-vcenter'>{customer.name}</Modal.Title>
+        <Button onClick={() => deleteCustomer()}>
+          <BsFillTrashFill className='ms-4' size={24} color={"red"} />
+        </Button>
       </Modal.Header>
       <Modal.Body className='px-3 py-2 mb-2'>
         <Tab.Container defaultActiveKey='first'>
