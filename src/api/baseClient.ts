981174f31276f05031aeaddfe8fcda72959fc27a
Erik <erik.grune@gmail.com>
2023-04-21 17:16:09 +0200

    faceplant

--- src/api/baseClient.ts
+++ src/api/baseClient.ts
@@ -1,9 +1,9 @@
 import { InitialUserState } from "../contexts/UserContext";
 
 export class BaseClient {
-  public baseUrl = import.meta.env.REACT_APP_API_URL;
+  envUrl = import.meta.env.REACT_APP_API_URL;
   getBaseUrl = (_: string, __: string | undefined) =>
-    import.meta.env.DEV ? "https://localhost:7178" : this.baseUrl;
+    import.meta.env.DEV ? "https://localhost:7178" : this.envUrl;
 
   protected transformOptions = async (options: RequestInit): Promise<RequestInit> => {
     let tokenInfo = InitialUserState.tokenInfo;
