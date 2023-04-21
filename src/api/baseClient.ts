eecec80efeafcbbf096dfcf27c7a1537ab76489e
Erik <erik.grune@gmail.com>
2023-04-21 16:13:11 +0200

    change title and add env url

--- src/api/baseClient.ts
+++ src/api/baseClient.ts
@@ -1,10 +1,8 @@
 import { InitialUserState } from "../contexts/UserContext";
 
-import.meta.env.DEV;
-
 export class BaseClient {
   getBaseUrl = (_: string, __: string | undefined) =>
-    import.meta.env.DEV ? "https://localhost:7178" : "http://legitURL.com";
+    import.meta.env.DEV ? "https://localhost:7178" : process.env.REACT_APP_API_URL ?? "";
 
   protected transformOptions = async (options: RequestInit): Promise<RequestInit> => {
     let tokenInfo = InitialUserState.tokenInfo;
