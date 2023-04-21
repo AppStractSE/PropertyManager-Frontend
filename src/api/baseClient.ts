b97dc110d4ee20a330b37e3b04b3c38bcd7e610b
Erik <erik.grune@gmail.com>
2023-04-21 22:33:56 +0200

    aaamagad...

--- src/api/baseClient.ts
+++ src/api/baseClient.ts
@@ -2,9 +2,7 @@ import { InitialUserState } from "../contexts/UserContext";
 
 export class BaseClient {
   getBaseUrl = (_: string, __: string | undefined) =>
-    import.meta.env.DEV
-      ? "https://localhost:7178"
-      : "https://https://propertymanger.azurewebsites.net/";
+    import.meta.env.DEV ? "https://localhost:7178" : "https://propertymanger.azurewebsites.net";
 
   protected transformOptions = async (options: RequestInit): Promise<RequestInit> => {
     let tokenInfo = InitialUserState.tokenInfo;
