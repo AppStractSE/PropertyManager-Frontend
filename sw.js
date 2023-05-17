176bfc4e5627b7b9d8f0deae8fe711972ee874e4
Erik <erik.grune@gmail.com>
2023-05-17 14:57:43 +0200

    fix pwa cache

--- sw.js
+++ sw.js
@@ -1,36 +1,51 @@
-const CACHE_NAME = "my-pwa-cache-v1";
+const CACHE_NAME = "my-pwa-cache-v2"; // Update cache version
 const urlsToCache = ["/", "/index.html", "/styles.css", "/script.js", "/images/logo.png"];
 
 self.addEventListener("install", (event) => {
   if (!event.request.headers.get("user-agent").includes("Mobile")) {
-  } else {
-  // Perform install steps
+    return; // Skip installation on non-mobile devices
+  }
+
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
-    }),
+    })
   );
-  }
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      // Remove outdated caches
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      // Cache hit - return response
       if (response) {
         return response;
       }
 
-      // Clone the request
       const fetchRequest = event.request.clone();
 
       return fetch(fetchRequest).then((response) => {
-        // Check if we received a valid response
-        if (!response || response.status !== 200 || response.type !== "basic") {
+        if (
+          !response ||
+          response.status !== 200 ||
+          response.type !== "basic"
+        ) {
           return response;
         }
 
-        // Clone the response
         const responseToCache = response.clone();
 
         caches.open(CACHE_NAME).then((cache) => {
@@ -39,6 +54,6 @@ self.addEventListener("fetch", (event) => {
 
         return response;
       });
-    }),
+    })
   );
 });
