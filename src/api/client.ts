5b009b0dcb7169fb29acb575285b8fe1168fc879
Lucas benjaminson <lucas_benjaminson@hotmail.com>
2023-05-17 14:11:42 +0200

    client.ts update with post subCategory

--- src/api/client.ts
+++ src/api/client.ts
@@ -1526,6 +1526,45 @@ export class Client extends BaseClient {
         return Promise.resolve<Periodic[]>(null as any);
     }
 
+    subCategory_PostSubCategory(request: PostSubCategoryRequestDto): Promise<SubCategory> {
+        let url_ = this.baseUrl + "/api/v1/SubCategory";
+        url_ = url_.replace(/[?&]$/, "");
+
+        const content_ = JSON.stringify(request);
+
+        let options_: RequestInit = {
+            body: content_,
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        };
+
+        return this.transformOptions(options_).then(transformedOptions_ => {
+            return this.http.fetch(url_, transformedOptions_);
+        }).then((_response: Response) => {
+            return this.processSubCategory_PostSubCategory(_response);
+        });
+    }
+
+    protected processSubCategory_PostSubCategory(response: Response): Promise<SubCategory> {
+        const status = response.status;
+        let _headers: any = {}; if (response.headers && response.headers.forEach) { response.headers.forEach((v: any, k: any) => _headers[k] = v); };
+        if (status === 200) {
+            return response.text().then((_responseText) => {
+            let result200: any = null;
+            result200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver) as SubCategory;
+            return result200;
+            });
+        } else if (status !== 200 && status !== 204) {
+            return response.text().then((_responseText) => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            });
+        }
+        return Promise.resolve<SubCategory>(null as any);
+    }
+
     team_GetAllTeams(): Promise<Team[]> {
         let url_ = this.baseUrl + "/api/v1/Team";
         url_ = url_.replace(/[?&]$/, "");
@@ -2235,6 +2274,12 @@ export interface PutCustomerRequestDto {
     areaId: string | undefined;
     teamId: string | undefined;
     address: string | undefined;
+}
+
+export interface PostSubCategoryRequestDto {
+    categoryId: string;
+    title: string | undefined;
+    reference: string | undefined;
 }
 
 export interface Team {
