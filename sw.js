c2c9c27127089c8223738f7cb54c743dbe27fbdc
Erik <erik.grune@gmail.com>
2023-05-30 17:24:58 +0200

    Fix service worker for pwa

--- sw.js
+++ sw.js
@@ -1,55 +1,56 @@
-// const CACHE_NAME = "my-pwa-cache-v2"; // Update cache version
-// const urlsToCache = ["/", "/index.html", "/styles.css", "/script.js", "/images/logo.png"];
+/* eslint-disable no-undef */
+const CACHE_NAME = "my-pwa-cache-v3"; // Update cache version
+const urlsToCache = ["/", "/index.html", "/styles.css", "/script.js", "/images/logo.png"];
 
-// self.addEventListener("install", (event) => {
-//   if (!event.request.headers.get("user-agent").includes("Mobile")) {
-//     return; // Skip installation on non-mobile devices
-//   }
+self.addEventListener("install", (event) => {
+  if (!event.request.headers.get("user-agent").includes("Mobile")) {
+    return; // Skip installation on non-mobile devices
+  }
 
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => {
-//       return cache.addAll(urlsToCache);
-//     }),
-//   );
-// });
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }),
+  );
+});
 
-// self.addEventListener("activate", (event) => {
-//   event.waitUntil(
-//     caches.keys().then((cacheNames) => {
-//       // Remove outdated caches
-//       return Promise.all(
-//         cacheNames.map((cacheName) => {
-//           if (cacheName !== CACHE_NAME) {
-//             return caches.delete(cacheName);
-//           }
-//         }),
-//       );
-//     }),
-//   );
-// });
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      // Remove outdated caches
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        }),
+      );
+    }),
+  );
+});
 
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((response) => {
-//       if (response) {
-//         return response;
-//       }
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
 
-//       const fetchRequest = event.request.clone();
+      const fetchRequest = event.request.clone();
 
-//       return fetch(fetchRequest).then((response) => {
-//         if (!response || response.status !== 200 || response.type !== "basic") {
-//           return response;
-//         }
+      return fetch(fetchRequest).then((response) => {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
 
-//         const responseToCache = response.clone();
+        const responseToCache = response.clone();
 
-//         caches.open(CACHE_NAME).then((cache) => {
-//           cache.put(event.request, responseToCache);
-//         });
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
 
-//         return response;
-//       });
-//     }),
-//   );
-// });
+        return response;
+      });
+    }),
+  );
+});
